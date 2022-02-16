26ccb9f2fd57c80896f8fcae75bd961e7530cb40
dongzhipeng <1760044214@qq.com>
2022-02-16 12:02:41 +0800

    sfsdfsd

--- src/api/menu/index.ts
+++ src/api/menu/index.ts
@@ -9,7 +9,7 @@ import request from '/@/utils/request';
  * 获取后端动态路由菜单(admin)
  * @link 参考：https://gitee.com/lyt-top/vue-next-admin-images/tree/master/menu
  * @param params 要传的参数值，非必传
- * @returns 返回接口数据
+ * @returns 返回接口数据sdfsf
  */
 export function getMenuAdmin(params?: object) {
 	return request({
